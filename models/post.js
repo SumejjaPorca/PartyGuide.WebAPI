f69060357cf7f03b39f04e9c8d88b9006fb4a674
SumejjaPorca <sumejja93@gmail.com>
2016-03-15 03:41:26 +0100

    Post.js fixed

--- models/post.js
+++ models/post.js
@@ -1,16 +1,38 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var PostSchema = new Schema({
-  barId:[Schema.Types.ObjectId],
-  title:String,
-  author:{
-    username: String,
-    _userId: Schema.Types.ObjectId,
-    adminOf: [[Schema.Types.ObjectId]]
+var Author = new Schema({
+  username: {
+    type: String,
+    required: true
   },
-  text: String,
-  dateCreated: Date,
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true
+  }
+});
+
+var PostSchema = new Schema({
+  barId:{
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  title:{
+    type: String,
+    required: true
+  },
+  author:{
+    type: Author,
+    required: true
+  },
+  text: {
+    type: String,
+    max: 1000
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now()
+  },
   date: Date
 });
 
